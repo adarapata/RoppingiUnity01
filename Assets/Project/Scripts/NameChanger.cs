26a1ab4b1e99fb9eb38bf373c2689415fe6a8cf1
adarapata <adarapata@e-f-b.jp>
2019-02-20 09:16:39 +0900

    first commit

---
+++ Assets/Project/Scripts/NameChanger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+public class NameChanger : MonoBehaviour
+{
+    [SerializeField]
+    private InputField _inputField = null;
+
+    [SerializeField]
+    private Text _display = null;
+
+    void Start()
+    {
+        _inputField.onValueChanged.AddListener(ShowDisplayName);
+    }
+
+    private void ShowDisplayName(string newName)
+    {
+        _display.text = newName.Length > 0 ? $"なまえ：{newName}" : "名無し";
+    }
+}
---
+++ Assets/Project/Tests/NameChangerTest.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class NameChangerTest
+    {
+        [UnityTest]
+        public IEnumerator FooUITestWithEnumeratorPasses()
+        {
+
+            yield return null;
+        }
+    }
+}
